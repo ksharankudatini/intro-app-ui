ca0c52186f90009cd745ecd4723193f9ed14e215
BASAVA K N <k-n.basava@soprasteria.com>
2026-08-31 17:04:09 +0530

    created basic setup and components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Login } from './feat/login/login';
+import { Home } from './feat/home/home';
+import { CreateAccount } from './feat/create-account/create-account';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: Home
+    },
+    {
+        path: 'login',
+        component: Login
+    },
+    {
+        path: 'create-account',
+        component: CreateAccount
+    }
+];
---
+++ src/app/feat/create-account/create-account.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-create-account',
+  styleUrl: './create-account.css',
+  templateUrl: './create-account.html',
+})
+export class CreateAccount {}
