3c38953dd72d35d1362a8e4ae699d46f9afce094
BASAVA K N <k-n.basava@soprasteria.com>
2026-08-31 18:32:58 +0530

    created create account page

--- src/app/feat/login/login.ts
+++ src/app/feat/login/login.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
-  imports: [],
+  imports: [ReactiveFormsModule],
   selector: 'app-login',
   styleUrl: './login.css',
   templateUrl: './login.html',
 })
-export class Login {}
+
+export class Login {
+  private readonly fb = inject(FormBuilder);
+
+  loginForm = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.loginForm.value);
+    // TODO: call auth service here
+  }
+}
